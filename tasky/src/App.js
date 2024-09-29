ad07885ca5b190bf5803c7b25be4b72994392d80
wcn1720811650 <1720811650@qq.com>
2024-09-29 15:47:28 +0100

    Mark task as done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -4,22 +4,31 @@ import Task from './components/Task';
 function App() {
   const [taskState, setTaskState ]  = useState({
     tasks: [
-      { id: 1, title:'Dishes', description: "Empty dishwasher", deadline: "Today"},
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" }
+      { id: 1, title:'Dishes', description: "Empty dishwasher", deadline: "Today", done:false},
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done:false },
+      { id: 3, title: "Tidy up", deadline: "Today", done:false }
     ]
   });
+  const doneHandler = (taskIndex) =>{
+      const tasks = [...taskState.tasks];
+      tasks[taskIndex].done = !tasks[taskIndex].done;
+      setTaskState({tasks});
+      console.log(`${taskIndex} ${tasks[taskIndex].done}`);
+    }
+  
   return (
     <div className="container">
       <h1>
         Tasky
       </h1>
-      {taskState.tasks.map((task) => (
+      {taskState.tasks.map((task,index) => (
         <Task 
-        title={task.title} 
-        deadline={task.description} 
-        description={task.deadline} 
-        key={task.id}
+          title={task.title} 
+          deadline={task.deadline} 
+          description={task.description} 
+          key={task.id}
+          done={task.done}
+          markDone = {() => doneHandler(index)}
         />
 
       ))}
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -1,11 +1,12 @@
 import React from "react";
 const Task = (props) =>{
     return (
-        <div className="card">  
+        <div className="card" style={{backgroundColor: props.done? 'lightgrey':'#5bb4c4'}}>  
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p>{props.children}</p>
             <p>{props.description}</p>
+            <button className="doneButton" onClick={props.markDone}>Done</button>
         </div>
     )
 
