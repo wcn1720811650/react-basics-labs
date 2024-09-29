83e4719d46780c31f30829ac7a8bf32ae127ea75
wcn1720811650 <1720811650@qq.com>
2024-09-29 15:22:03 +0100

    Rendered list of tasks using map

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -11,10 +11,17 @@ function App() {
   });
   return (
     <div className="container">
-      <h1>Tasky</h1>
-      <Task title={taskState.tasks[0].title} deadline={taskState.tasks[0].deadline} description={taskState.tasks[0].description} />
-      <Task title={taskState.tasks[1].title} deadline={taskState.tasks[1].deadline} description={taskState.tasks[1].description} />
-      <Task title={taskState.tasks[2].title} deadline={taskState.tasks[2].deadline} description={taskState.tasks[2].description} />
+      <h1>
+        Tasky
+      </h1>
+      {taskState.tasks.map((task) => (
+        <Task 
+        title={task.title} 
+        deadline={task.description} 
+        description={task.deadline} />
+
+      ))}
+      
     </div>
   );
 }
